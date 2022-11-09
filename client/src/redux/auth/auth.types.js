5150ff7b55777236ebd4b9995938484b316afa13
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-09 09:20:55 +0530

    created redux store template

---
+++ client/src/redux/auth/auth.types.js
@@ -0,0 +1,4 @@
+export const SIGN_IN_LOADING = "signin/loading";
+export const SIGN_IN_SUCCESS = "signin/success";
+export const SIGN_IN_ERROR = "signin/error";
+export const SIGN_OUT = "signout";
---
+++ client/src/redux/store.js
@@ -0,0 +1,7 @@
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({});
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
