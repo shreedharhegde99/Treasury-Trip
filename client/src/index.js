feb9d2e5b585078c3c5a72c1b2de6a39df1b5b00
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-09 10:02:55 +0530

    added chakra-provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ChakraProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ChakraProvider>
 );
