f11cdd86ce92d3f9ec491c5bbc9195467ba1d23d
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-10 00:38:22 +0530

    responsiveness updates

--- client/src/components/AirportTaxi/AirportTaxiCards.jsx
+++ client/src/components/AirportTaxi/AirportTaxiCards.jsx
@@ -13,10 +13,9 @@ export default function AirportTaxiCards({
   cancellation,
 }) {
   return (
-    <Fragment>
-      <Box p="4" border="1px" borderColor="black" w="max-content">
+      <Box p="4" border="1px" borderColor="black" minW="sm">
         <Box>
-          <Heading as="h2" size="sm">
+          <Heading as="h2" size="md">
             {type}
           </Heading>
           <Text>{vehicle}</Text>
@@ -24,32 +23,31 @@ export default function AirportTaxiCards({
         <Box>
           <HStack>
             <Box>
-              <BsFillPersonFill />
+              <BsFillPersonFill size="1.5rem" />
             </Box>
             <Box>{passengers}</Box>
           </HStack>
 
           <HStack>
             <Box>
-              <FaSuitcase />
+              <FaSuitcase size="1.5rem"  />
             </Box>
             <Box>{bags}</Box>
           </HStack>
           <HStack>
             <Box>
-              <HiOutlineCheckCircle />
+              <HiOutlineCheckCircle size="1.5rem"  />
             </Box>
             <Box>{included}</Box>
           </HStack>
           <HStack>
             <Box>
-              <BsCheck />
+              <BsCheck size="1.5rem"  />
             </Box>
             <Box>{cancellation}</Box>
           </HStack>
           <Button variant="solid" colorScheme="telegram" w="16" h="8">Search</Button>
         </Box>
       </Box>
-    </Fragment>
   );
 }
