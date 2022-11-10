a44f158b3e671901be547ffe3646b3e81ee8b7d1
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-10 15:18:35 +0530

    created download app component and subscribe letter component

--- client/src/pages/AirportTaxis.jsx
+++ client/src/pages/AirportTaxis.jsx
@@ -33,6 +33,8 @@ import images from "../assets/airportTaxi.json";
 import AirportTransfers from "../components/AirportTaxi/AirportTransfers";
 import AirportTaxiCards from "../components/AirportTaxi/AirportTaxiCards";
 import TaxiFaq from "../components/AirportTaxi/TaxiFaq";
+import DownloadApp from "../components/AirportTaxi/DownloadApp";
+import SubscribeLetter from "../components/AirportTaxi/SubscribeLetter";
 
 const { how_it_works, taxi, driver, destination } = images;
 export default function AirportTaxi() {
@@ -367,117 +369,121 @@ export default function AirportTaxi() {
           </Flex>
         </Show>
         <Show above="md">
-          <HStack py="6">
-            <Box
-              bg={`${passengers === 1 ? "blue" : "white"}`}
-              color={`${passengers === 1 ? "white" : "blue"}`}
-              border="1px"
-              borderColor="blue.300"
-              p="2"
-              onClick={() => setPassengers(1)}
-              cursor="pointer"
-            >
-              1-3 passengers
-            </Box>
-            <Box
-              bg={`${passengers === 3 ? "blue" : "white"}`}
-              color={`${passengers === 3 ? "white" : "blue"}`}
-              border="1px"
-              borderColor="blue.300"
-              p="2"
-              onClick={() => setPassengers(3)}
-              cursor="pointer"
-            >
-              4-7 passengers
-            </Box>
-            <Box
-              bg={`${passengers === "all" ? "blue" : "white"}`}
-              color={`${passengers === "all" ? "white" : "blue"}`}
-              border="1px"
-              borderColor="blue.300"
-              p="2"
-              onClick={() => setPassengers("all")}
-              cursor="pointer"
-            >
-              All taxis
-            </Box>
-          </HStack>
-          <Grid templateColumns="repeat(2,1fr)" gap="10">
-            <GridItem
-              display={`${
-                passengers === 1 || passengers === "all" ? "content" : "none"
-              }`}
-            >
-              <AirportTaxiCards
-                type="Standard"
-                vehicle="Skoda octavia or similar"
-                passengers=" 3 passengers"
-                bags="2 standard bags"
-                included="Meet & Greet included"
-                cancellation="Free cancellation"
-              />
-            </GridItem>
-            <GridItem
-              display={`${
-                passengers === 1 || passengers === "all" ? "content" : "none"
-              }`}
-            >
-              <AirportTaxiCards
-                type="Executive"
-                vehicle="Mercedes-benz E-Class or similar"
-                passengers=" 3 passengers"
-                bags="2 standard bags"
-                included="Meet & Greet included"
-                cancellation="Free cancellation"
-              />
-            </GridItem>
-            <GridItem
-              display={`${
-                passengers === 3 || passengers === "all" ? "content" : "none"
-              }`}
-            >
-              <AirportTaxiCards
-                type="People carrier"
-                vehicle="Peugeot 5008 or similar"
-                passengers=" 5 passengers"
-                bags="5 standard bags"
-                included="Meet & Greet included"
-                cancellation="Free cancellation"
-              />
-            </GridItem>
-            <GridItem
-              display={`${
-                passengers === 3 || passengers === "all" ? "content" : "none"
-              }`}
-            >
-              <AirportTaxiCards
-                type="Executive people carrier"
-                vehicle="Mercedes Benz V-Class or similar"
-                passengers=" 6 passengers"
-                bags="6 standard bags"
-                included="Meet & Greet included"
-                cancellation="Free cancellation"
-              />
-            </GridItem>
-            <GridItem
-              display={`${
-                passengers === 3 || passengers === "all" ? "content" : "none"
-              }`}
-            >
-              <AirportTaxiCards
-                type="Large people carrier"
-                vehicle="Ford Tourneo or similar"
-                passengers=" 7 passengers"
-                bags="7 standard bags"
-                included="Meet & Greet included"
-                cancellation="Free cancellation"
-              />
-            </GridItem>
-          </Grid>
+          <Box maxW="container.lg" m="auto">
+            <HStack py="6">
+              <Box
+                bg={`${passengers === 1 ? "blue" : "white"}`}
+                color={`${passengers === 1 ? "white" : "blue"}`}
+                border="1px"
+                borderColor="blue.300"
+                p="2"
+                onClick={() => setPassengers(1)}
+                cursor="pointer"
+              >
+                1-3 passengers
+              </Box>
+              <Box
+                bg={`${passengers === 3 ? "blue" : "white"}`}
+                color={`${passengers === 3 ? "white" : "blue"}`}
+                border="1px"
+                borderColor="blue.300"
+                p="2"
+                onClick={() => setPassengers(3)}
+                cursor="pointer"
+              >
+                4-7 passengers
+              </Box>
+              <Box
+                bg={`${passengers === "all" ? "blue" : "white"}`}
+                color={`${passengers === "all" ? "white" : "blue"}`}
+                border="1px"
+                borderColor="blue.300"
+                p="2"
+                onClick={() => setPassengers("all")}
+                cursor="pointer"
+              >
+                All taxis
+              </Box>
+            </HStack>
+            <Grid templateColumns="repeat(2,1fr)" gap="10">
+              <GridItem
+                display={`${
+                  passengers === 1 || passengers === "all" ? "content" : "none"
+                }`}
+              >
+                <AirportTaxiCards
+                  type="Standard"
+                  vehicle="Skoda octavia or similar"
+                  passengers=" 3 passengers"
+                  bags="2 standard bags"
+                  included="Meet & Greet included"
+                  cancellation="Free cancellation"
+                />
+              </GridItem>
+              <GridItem
+                display={`${
+                  passengers === 1 || passengers === "all" ? "content" : "none"
+                }`}
+              >
+                <AirportTaxiCards
+                  type="Executive"
+                  vehicle="Mercedes-benz E-Class or similar"
+                  passengers=" 3 passengers"
+                  bags="2 standard bags"
+                  included="Meet & Greet included"
+                  cancellation="Free cancellation"
+                />
+              </GridItem>
+              <GridItem
+                display={`${
+                  passengers === 3 || passengers === "all" ? "content" : "none"
+                }`}
+              >
+                <AirportTaxiCards
+                  type="People carrier"
+                  vehicle="Peugeot 5008 or similar"
+                  passengers=" 5 passengers"
+                  bags="5 standard bags"
+                  included="Meet & Greet included"
+                  cancellation="Free cancellation"
+                />
+              </GridItem>
+              <GridItem
+                display={`${
+                  passengers === 3 || passengers === "all" ? "content" : "none"
+                }`}
+              >
+                <AirportTaxiCards
+                  type="Executive people carrier"
+                  vehicle="Mercedes Benz V-Class or similar"
+                  passengers=" 6 passengers"
+                  bags="6 standard bags"
+                  included="Meet & Greet included"
+                  cancellation="Free cancellation"
+                />
+              </GridItem>
+              <GridItem
+                display={`${
+                  passengers === 3 || passengers === "all" ? "content" : "none"
+                }`}
+              >
+                <AirportTaxiCards
+                  type="Large people carrier"
+                  vehicle="Ford Tourneo or similar"
+                  passengers=" 7 passengers"
+                  bags="7 standard bags"
+                  included="Meet & Greet included"
+                  cancellation="Free cancellation"
+                />
+              </GridItem>
+            </Grid>
+          </Box>
         </Show>
       </Box>
 
-      <TaxiFaq/>
+      <TaxiFaq />
+      <DownloadApp />
+      <SubscribeLetter />
     </Fragment>
   );
 }
