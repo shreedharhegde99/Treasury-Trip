f608cb997213c61e3d50d23a8c7f61c32f2ca4a6
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-10 11:24:26 +0530

    updated airport taxi cards and made responsive

--- client/src/pages/AirportTaxis.jsx
+++ client/src/pages/AirportTaxis.jsx
@@ -16,6 +16,7 @@ import {
   Radio,
   RadioGroup,
   Select,
+  Show,
   Stack,
   Text,
   VStack,
@@ -27,7 +28,6 @@ import { BsPersonFill } from "react-icons/bs";
 import { GoCheck } from "react-icons/go";
 import { GrCreditCard } from "react-icons/gr";
 import { RiPlaneFill } from "react-icons/ri";
-import { FaCar } from "react-icons/fa";
 
 import images from "../assets/airportTaxi.json";
 import AirportTransfers from "../components/AirportTaxi/AirportTransfers";
@@ -38,6 +38,7 @@ export default function AirportTaxi() {
   const [trip, setTrip] = useState("oneway");
   const [hr, setHr] = useState("");
   const [min, setMin] = useState("");
+  const [passengers, setPassengers] = useState(1);
   return (
     <Fragment>
       <Box p="4">
@@ -315,53 +316,164 @@ export default function AirportTaxi() {
       </VStack>
 
       <Box px="4" py="6">
-        <Box>
-          <Heading as="h2" size="sm" py="6">
+        <Box textAlign={{ md: "center" }}>
+          <Heading as="h2" size={{ base: "sm", md: "lg" }} py="6">
             Airport taxis for any kind of trip
           </Heading>
         </Box>
-        <Flex overflow="scroll" gap="6" >
-        <AirportTaxiCards 
-        type="Standard"
-        vehicle="Skoda octavia or similar"
-        passengers=" 3 passengers" 
-        bags="2 standard bags"
-        included="Meet & Greet included"
-        cancellation="Free cancellation"
-        />
-         <AirportTaxiCards 
-        type="Executive"
-        vehicle="Mercedes-benz E-Class or similar"
-        passengers=" 3 passengers"
-        bags="2 standard bags"
-        included="Meet & Greet included"
-        cancellation="Free cancellation"
-        />
-        <AirportTaxiCards 
-        type="People carrier"
-        vehicle="Peugeot 5008 or similar"
-        passengers=" 5 passengers"
-        bags="5 standard bags"
-        included="Meet & Greet included"
-        cancellation="Free cancellation"
-        />
-        <AirportTaxiCards 
-        type="Executive people carrier"
-        vehicle="Mercedes Benz V-Class or similar"
-        passengers=" 6 passengers"
-        bags="6 standard bags"
-        included="Meet & Greet included"
-        cancellation="Free cancellation"
-        />
-         <AirportTaxiCards 
-        type="Large people carrier"
-        vehicle="Ford Tourneo or similar"
-        passengers=" 7 passengers"
-        bags="7 standard bags"
-        included="Meet & Greet included"
-        cancellation="Free cancellation"
-        />
-        </Flex>
+        <Show below="md">
+          <Flex overflow="scroll" gap="6">
+            <AirportTaxiCards
+              type="Standard"
+              vehicle="Skoda octavia or similar"
+              passengers=" 3 passengers"
+              bags="2 standard bags"
+              included="Meet & Greet included"
+              cancellation="Free cancellation"
+            />
+            <AirportTaxiCards
+              type="Executive"
+              vehicle="Mercedes-benz E-Class or similar"
+              passengers=" 3 passengers"
+              bags="2 standard bags"
+              included="Meet & Greet included"
+              cancellation="Free cancellation"
+            />
+            <AirportTaxiCards
+              type="People carrier"
+              vehicle="Peugeot 5008 or similar"
+              passengers=" 5 passengers"
+              bags="5 standard bags"
+              included="Meet & Greet included"
+              cancellation="Free cancellation"
+            />
+            <AirportTaxiCards
+              type="Executive people carrier"
+              vehicle="Mercedes Benz V-Class or similar"
+              passengers=" 6 passengers"
+              bags="6 standard bags"
+              included="Meet & Greet included"
+              cancellation="Free cancellation"
+            />
+            <AirportTaxiCards
+              type="Large people carrier"
+              vehicle="Ford Tourneo or similar"
+              passengers=" 7 passengers"
+              bags="7 standard bags"
+              included="Meet & Greet included"
+              cancellation="Free cancellation"
+            />
+          </Flex>
+        </Show>
+        <Show above="md">
+          <HStack py="6">
+            <Box
+              bg={`${passengers === 1 ? "blue" : "white"}`}
+              color={`${passengers === 1 ? "white" : "blue"}`}
+              border="1px"
+              borderColor="blue.300"
+              p="2"
+              onClick={() => setPassengers(1)}
+              cursor="pointer"
+            >
+              1-3 passengers
+            </Box>
+            <Box
+              bg={`${passengers === 3 ? "blue" : "white"}`}
+              color={`${passengers === 3 ? "white" : "blue"}`}
+              border="1px"
+              borderColor="blue.300"
+              p="2"
+              onClick={() => setPassengers(3)}
+              cursor="pointer"
+            >
+              4-7 passengers
+            </Box>
+            <Box
+              bg={`${passengers === "all" ? "blue" : "white"}`}
+              color={`${passengers === "all" ? "white" : "blue"}`}
+              border="1px"
+              borderColor="blue.300"
+              p="2"
+              onClick={() => setPassengers("all")}
+              cursor="pointer"
+            >
+              All taxis
+            </Box>
+          </HStack>
+          <Grid templateColumns="repeat(2,1fr)" gap="10">
+            <GridItem
+              display={`${
+                passengers === 1 || passengers === "all" ? "content" : "none"
+              }`}
+            >
+              <AirportTaxiCards
+                type="Standard"
+                vehicle="Skoda octavia or similar"
+                passengers=" 3 passengers"
+                bags="2 standard bags"
+                included="Meet & Greet included"
+                cancellation="Free cancellation"
+              />
+            </GridItem>
+            <GridItem
+              display={`${
+                passengers === 1 || passengers === "all" ? "content" : "none"
+              }`}
+            >
+              <AirportTaxiCards
+                type="Executive"
+                vehicle="Mercedes-benz E-Class or similar"
+                passengers=" 3 passengers"
+                bags="2 standard bags"
+                included="Meet & Greet included"
+                cancellation="Free cancellation"
+              />
+            </GridItem>
+            <GridItem
+              display={`${
+                passengers === 3 || passengers === "all" ? "content" : "none"
+              }`}
+            >
+              <AirportTaxiCards
+                type="People carrier"
+                vehicle="Peugeot 5008 or similar"
+                passengers=" 5 passengers"
+                bags="5 standard bags"
+                included="Meet & Greet included"
+                cancellation="Free cancellation"
+              />
+            </GridItem>
+            <GridItem
+              display={`${
+                passengers === 3 || passengers === "all" ? "content" : "none"
+              }`}
+            >
+              <AirportTaxiCards
+                type="Executive people carrier"
+                vehicle="Mercedes Benz V-Class or similar"
+                passengers=" 6 passengers"
+                bags="6 standard bags"
+                included="Meet & Greet included"
+                cancellation="Free cancellation"
+              />
+            </GridItem>
+            <GridItem
+              display={`${
+                passengers === 3 || passengers === "all" ? "content" : "none"
+              }`}
+            >
+              <AirportTaxiCards
+                type="Large people carrier"
+                vehicle="Ford Tourneo or similar"
+                passengers=" 7 passengers"
+                bags="7 standard bags"
+                included="Meet & Greet included"
+                cancellation="Free cancellation"
+              />
+            </GridItem>
+          </Grid>
+        </Show>
       </Box>
     </Fragment>
   );
