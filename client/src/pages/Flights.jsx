370aa3833bdfc80456aa79d8f64d0dd982615237
unknown <iamfarmanmalik@gmail.com>
2022-11-10 23:04:07 +0530

    file added

--- client/src/pages/Flights.jsx
+++ client/src/pages/Flights.jsx
@@ -1,20 +1,29 @@
-import { Container } from "@chakra-ui/react";
+import { Center, Container } from "@chakra-ui/react";
 import React from "react";
 import { BookFlight } from "../components/Flights/Bookflight";
 import { Coronawarning } from "../components/Flights/Coronawarning";
+import { Faqpage } from "../components/Flights/Faqpage";
 import { Feature } from "../components/Flights/Feature";
+import { Flyword } from "../components/Flights/Flyword";
 import { Popularflight } from "../components/Flights/Popularflight";
 import { Trendingflight } from "../components/Flights/Trendingflight";
 export const Flights=()=>{
     return(
-        <Container maxW={"100%"}>
-           <Container maxW={"90%"}>
+        <Container maxW={"100vw"}  overflow="hidden" minW={"100vw"} bgColor="rgb(242,242,242)" pt="10px">
+         <Center>
+         <Container maxW={{base:"95vw",md:"90vw"}} >
            <Coronawarning/>
            <BookFlight/>
-           <Popularflight/>
-           <Trendingflight/>
+          <Center > <Popularflight/></Center>
+           <Center><Trendingflight/></Center>
            <Feature/>
+           <Flyword/>
+            <Faqpage/>
+
            </Container>
+          
+         </Center>
+
         </Container>
     )
 }
