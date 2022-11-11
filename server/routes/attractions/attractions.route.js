8850006b6facc164a064fbf7a79324a5c4e8e456
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-11 13:06:43 +0530

    fixed response data error

--- server/routes/attractions/attractions.route.js
+++ server/routes/attractions/attractions.route.js
@@ -4,17 +4,23 @@ const express = require("express");
 const app = express.Router();
 
 app.get("/", async (req, res) => {
-  console.log("request for attractions");
+  // console.log("request for attractions");
   const { page, country } = req.query;
-  console.log(page);
-  if(country){
-
+  // console.log(page);
+  if (country) {
     const countryData = await Attraction.findOne({ country: country });
-    // console.log(countryData.data);
-    res.send(countryData.data);
-    return ;
+
+    if (countryData?.data) {
+      res.status(200).send(countryData.data);
+    } else {
+      res
+        .status(404)
+        .send({ message: "Data not available for country requested" });
+    }
+
+    return;
   }
-  res.send({message:"Please provide the country name"})
+  res.send({ message: "Please provide the country name" });
 });
 
 module.exports = app;
