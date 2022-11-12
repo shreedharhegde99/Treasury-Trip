4121166c9d62c1809e8ee170ca250a2ff45998cc
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-13 00:20:47 +0530

    fixed active component border color

--- client/src/components/Navbar/NavbarR.jsx
+++ client/src/components/Navbar/NavbarR.jsx
@@ -1,17 +1,18 @@
 import { Box, Hide } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { IoIosBed } from "react-icons/io";
 import { AiFillCar } from "react-icons/ai";
 import { MdFlightTakeoff } from "react-icons/md";
 import { FaPlaceOfWorship, FaTaxi } from "react-icons/fa";
 import NavbarButton from "./NavbarButton";
+
 const NavbarR = () => {
   const [active, setActive] = useState("stays");
-  const handleActive = (val) => {
-    setActive(val);
-    console.log(val);
-  };
+  const { pathname } = useLocation();
+  useEffect(() => {
+    setActive(pathname.split("/").join(""));
+  }, [pathname]);
 
   return (
     <>
@@ -22,42 +23,36 @@ const NavbarR = () => {
             pb="15px"
             m="auto"
             fontSize={["sm", "sm", "md"]}
-            // border={"1px solid"}
             display="flex"
             justifyContent="center"
             gap={20}
           >
             <NavbarButton
               active={active}
-              setActive={handleActive}
               text="Stays"
               link="stays"
               icon={<IoIosBed />}
             />
             <NavbarButton
               active={active}
-              setActive={handleActive}
               text="Flights"
               link="flights"
               icon={<MdFlightTakeoff />}
             />
             <NavbarButton
               active={active}
-              setActive={handleActive}
               text="Car Rentals"
               link="car-rentals"
               icon={<AiFillCar />}
             />
             <NavbarButton
               active={active}
-              setActive={handleActive}
               text="Attractions"
               link="attractions"
               icon={<FaPlaceOfWorship />}
             />
             <NavbarButton
               active={active}
-              setActive={handleActive}
               text="Airport+Taxis"
               link="taxis"
               icon={<FaTaxi />}
