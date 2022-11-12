33a8fcabd869d8d7e4de1d8cdc0a139827a8037d
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-13 03:41:35 +0530

    modified model

--- server/routes/flights/flights.model.js
+++ server/routes/flights/flights.model.js
@@ -2,18 +2,14 @@ const mongoose = require("mongoose");
 const flightSchema = new mongoose.Schema({
   from: String,
   to: String,
-  data: [
-    {
-      departure: String,
-      loc1: String,
-      duration: String,
-      arrive: String,
-      loc2: String,
-      airline: String,
-      prc: String,
-      _id: String,
-    },
-  ],
+  departure: String,
+  loc1: String,
+  duration: String,
+  arrive: String,
+  loc2: String,
+  airline: String,
+  prc: String,
+  _id: String,
 });
 
 const Flight = mongoose.model("flight", flightSchema);
