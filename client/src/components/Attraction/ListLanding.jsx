ff3d02d074e3135d37d9efbd768426406817760d
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-11-12 19:41:02 +0530

    day5

--- client/src/components/Attraction/ListLanding.jsx
+++ client/src/components/Attraction/ListLanding.jsx
@@ -1,9 +1,8 @@
 import {Box, Button, HStack, Input, InputGroup, Image,InputLeftElement, Stack} from "@chakra-ui/react"
 import {FiSearch} from "react-icons/fi"
 import {BiChevronRight} from "react-icons/bi"
-import {MdTimer} from "react-icons/md"
 import {BsCalendarCheck} from "react-icons/bs"
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, Tab } from '@chakra-ui/react'
 import "./ListLanding.css"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
@@ -11,13 +10,14 @@ import { getCityData } from "../../redux/attractions/attractions.action"
 export default function ListLanding(){
     const {cityData}=useSelector(state=>state.attraction)
     const [data,setData]=useState(cityData)
+    // const [text,setText]=useState('')
 
     const dispatch=useDispatch()
     // console.log(dataLoaded);
 
     
     const handleSearch=()=>{
-        dispatch(getCityData("Bangalore"))
+        dispatch(getCityData('bangalore'))
         // setData(cityData)
          console.log("inside handle search");
      }
@@ -36,10 +36,22 @@ export default function ListLanding(){
             let [rs,prices]=b.price.trim().split('.')
             price=price.split(",").join("");
             prices=prices.split(",").join("");
-                console.log(Number(price),Number(prices))
+                // console.log(Number(price),Number(prices))
             
              return Number(price)-Number(prices)
 
+        })
+        setData(updatedData)
+    }
+
+    const highToLow=()=>{
+        let updatedData=[...data].sort((a,b)=>{
+            let [Rs,price]=a.price.trim().split('.')
+            let [rs,prices]=b.price.trim().split('.')
+            price=price.split(",").join("");
+            prices=prices.split(",").join("");
+                // console.log(Number(price),Number(prices))          
+             return Number(prices)-Number(price)
         })
         setData(updatedData)
     }
@@ -47,9 +59,10 @@ export default function ListLanding(){
 
     return (
        <Box>
+          <Box ml={'10%'} pt={'5%'} style={{fontWeight:'bold',fontSize:'25px'}}>Bangalore Attractions</Box>
          <HStack align={'start'}>  
             {/* Left-Box */}
-            <Box mt={'100px'} ml={'10%'} mr={'5%'} w="28%">
+            <Box mt={'20px'} ml={'10%'} mr={'5%'} w="28%">
                 <Box border="4px" borderColor={"orange"} borderRadius={'10px'}>
                     <InputGroup>
                         <InputLeftElement><FiSearch/></InputLeftElement>
@@ -71,16 +84,19 @@ export default function ListLanding(){
                     </HStack> 
                     <h1 style={{fontWeight:'500',fontSize:'15px',color:'#1A1A1A',marginTop:'5%'}}>Price</h1>
                     <HStack>
-                        <input type="checkbox" /> <h1>Rs 0 - Rs 1,634</h1>                              
+                        <input type="checkbox" /> <h1>Rs. 0 - Rs. 1,669</h1>                              
                     </HStack>
                     <HStack>
-                        <input type="checkbox" /> <h1>Rs 0 - Rs 1,634</h1> 
+                        <input type="checkbox" /> <h1>Rs. 1,669 - Rs. 3,338</h1> 
                     </HStack> 
                     <HStack>
-                        <input type="checkbox" /> <h1>Rs 0 - Rs 1,634</h1>                              
+                        <input type="checkbox" /> <h1>Rs. 3,338 - Rs. 6,259</h1>                              
                     </HStack>
                     <HStack>
-                        <input type="checkbox" /> <h1>Rs 0 - Rs 1,634</h1> 
+                        <input type="checkbox" /> <h1>Rs. 6,259 - Rs. 10,432</h1> 
+                    </HStack> 
+                    <HStack>
+                        <input type="checkbox" /> <h1>Rs. 10,432 +</h1> 
                     </HStack> 
                     <h1 style={{fontWeight:'500',fontSize:'15px',color:'#1A1A1A',marginTop:'5%'}}>Other</h1>
                     <HStack>
@@ -89,48 +105,41 @@ export default function ListLanding(){
                 </Box>
             </Box>
             {/* right box */}
-            <Box w="50%" pt={'100px'} >
+            <Box w="50%" pt={'20px'} >
               
                <Tabs variant='soft-rounded' colorScheme='white' marginLeft={'-5%'}>
                     <TabList w="100%" mt={'-10px'}>
                         <HStack>
-                            <Tab className="hover">Our top picks</Tab>
+                            <Tab onClick={highToLow} className="hover">Our top picks</Tab>
                             <Tab className="hover">Most popular</Tab>
                             <Tab onClick={sortLowToHigh} className="hover">Lowest price</Tab>
                         </HStack>
                     </TabList>
-                    {/* <TabPanels>
-                        <TabPanel><OurTopPicks/></TabPanel>
-                        <TabPanel><MostPopular/></TabPanel>
-                        <TabPanel><LowestPrice/></TabPanel>
-                    </TabPanels> */}
                 </Tabs>             
                 {/* cards starts from here */}
                 <Box marginLeft={'-7%'} mt={'5px'}>  
                     <Stack gap={3}>
                                 {
-                                    cityData.map((el)=>{
-                                    return  <Box key={el._id} w='100%' borderRadius='10px' h='auto' border={'1px solid lightgrey'} cursor='pointer'>
-                                        <HStack>
-                                            <Box w='25%' m={'15px'} >
-                                                <Image w='100%' borderRadius={'10px'} src={el.image}/>
-                                            </Box>
-                                            <Box w='70%'>
-                                                <p style={{color:'#1A1A1A',fontSize:'14px',marginTop:'15px'}}>{el.city}</p>
-                                                <h1 style={{fontWeight:'bold',fontSize:'20px'}}>{el.title}</h1>
-                                                <p style={{color:'#1A1A1A',fontSize:'14px',marginBottom:'15px'}}>{el.desc}</p>
-                                                <Box style={{color:'#1A1A1A',fontSize:'14px',marginBottom:'2px'}}><HStack><MdTimer/> {el.duration}?<p>Duration 2 hours</p>:''</HStack></Box>
-                                                <Box style={{fontSize:'14px',color:'#008234'}}><HStack><BsCalendarCheck/> <p>{el.cancellation}</p></HStack></Box>
-                                                <Box className="rightalign">From <span><p>{el.price}</p></span> </Box>
-                                                <Button bg={'#00000'} className="seeAvail">See availability <BiChevronRight/></Button>
-                                            </Box>
-    
-                                        </HStack>
-                                    </Box>
+                                    data.map((el)=>{
+                                        return  <Box className="textHover" key={el._id} w='100%' borderRadius='10px' h='auto' border={'1px solid lightgrey'} cursor='pointer'>
+                                            <HStack>
+                                                <Box w='25%' m={'15px'} >
+                                                    <Image w='100%' borderRadius={'10px'} src={el.image} loading="lazy"/>
+                                                </Box>
+                                                <Box mt={'-20'} w='70%'>
+                                                    <p style={{color:'#1A1A1A',fontSize:'14px',marginTop:'5px'}}>{el.city}</p>
+                                                    <h1 style={{fontWeight:'bold',fontSize:'20px'}}>{el.title}</h1>
+                                                    <p style={{color:'#1A1A1A',fontSize:'14px',marginBottom:'15px'}}>{el.desc}</p>
+                                                    {/* <Box style={{color:'#1A1A1A',fontSize:'14px',marginBottom:'2px'}}><HStack><MdTimer/>{el.duration}?<p>Duration 2 hours</p>:''</HStack></Box> */}
+                                                    <Box style={{fontSize:'14px',color:'#008234'}}><HStack><BsCalendarCheck/> <p>{el.cancellation}</p></HStack></Box>
+                                                    <Box className="rightalign">From <span><p>{el.price}</p></span> </Box>
+                                                    <Button bg={'#00000'} className="seeAvail">See availability <BiChevronRight/></Button>
+                                                </Box>
+        
+                                            </HStack>
+                                        </Box>
                                    })
-                                }
-                             
-                       
+                                }                            
                     </Stack>
                 </Box>    
             </Box>
