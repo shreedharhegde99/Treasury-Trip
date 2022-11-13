af3fd7b135f175ca2afeae03b1606fa42397672b
unknown <iamfarmanmalik@gmail.com>
2022-11-13 14:16:45 +0530

    file added

--- client/src/allRoutes/AllRoutes.jsx
+++ client/src/allRoutes/AllRoutes.jsx
@@ -12,13 +12,13 @@ import { FlightSearch } from "../pages/FlightSearch";
 import NotFound from "../pages/NotFound";
 import { PersonalDetails } from "../pages/PersonalDetails";
 import Stays from "../pages/Stays";
-<<<<<<< HEAD
+
 import { Ticket } from "../pages/Ticket";
 
-=======
+
 import Airport_Taxi from "../pages/AirportTaxis"
 import ListLanding from "../components/Attraction/ListLanding";
->>>>>>> b8cc9915c3ccc2713c7cc8a388b9b631f9168fef
+
 
 export default function AllRoutes() {
   return (
@@ -27,13 +27,11 @@ export default function AllRoutes() {
       <Route path="/stays" element={<Stays />} />
       <Route path="/flights" element={<Flights />} />
       <Route path="/attractions" element={<Attractions />} />
-<<<<<<< HEAD
-      {/* <Route path="/taxis" element={<Airport_Taxi />} /> */}
-=======
+
       <Route path="/attractions/:city" element={<ListLanding />} />
       
       <Route path="/taxis" element={<Airport_Taxi />} />
->>>>>>> b8cc9915c3ccc2713c7cc8a388b9b631f9168fef
+
       <Route path="/login" element={<Login/>} />
     <Route path="/invoice/:id" element={<Ticket/>}/>
       <Route path="/taxis" element={<AirportTaxi />} />
