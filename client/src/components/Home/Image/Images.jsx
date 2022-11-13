cc40c461732a0d67bd05a9b23f024539a6714274
unknown <iamfarmanmalik@gmail.com>
2022-11-13 18:54:43 +0530

    file added

--- client/src/components/Home/Image/Images.jsx
+++ client/src/components/Home/Image/Images.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import Header from '../Top/Header'
 
 export default function Images() {
-  return (<Container  h={"400px"} minW="100vw" bgImage={"https://q-xx.bstatic.com/xdata/images/xphoto/3000x1033/177464000.jpeg?k=e74f22d059a85966c866bcf068f5a2a49f08ace871a3f459f5d71569c499896a&o="}>
+ 
+  return (<Container borderTop={"2px"} bgSize={{base:"cover",md:"cover"} }borderColor="white" h={{base:"300px",md:"400px"}} minW="100vw" bgImage={"https://q-xx.bstatic.com/xdata/images/xphoto/3000x1033/177464000.jpeg?k=e74f22d059a85966c866bcf068f5a2a49f08ace871a3f459f5d71569c499896a&o="}>
  <VStack w="full" textAlign={"left"}>
- <Heading   mt="100px" fontSize={"59px"} color={"white"}>Late Escape Deals
+ <Heading   mt={{base:"40px",md:"100px"}} fontSize={{base:"39px",md:"59px"}} color={"white"}>Late Escape Deals.
 </Heading>
-<Text w="600px" textAlign={"center"} color={"silver"}>There’s always time for one more trip. Discover deals on stays between 1 October 2022 - 3 January 2023</Text>
+<Text w="600px" display={{base:"none",md:"unset"}} textAlign={"center"} color={"silver"}>There’s always time for one more trip. Discover deals on stays between 1 October 2022 - 3 January 2023</Text>
  </VStack>
 
 <Header/>
--- client/src/components/Home/Top/Header.jsx
+++ client/src/components/Home/Top/Header.jsx
@@ -8,7 +8,7 @@ export default function Header(){
         navigate(`/hotels/${place}`)
     }
     return(<Center >
-        <Stack border="2px" borderColor={"silver"} mt="20px" borderRadius={"10px"} p="10px" bgColor={"rgb(0,34,79)"} direction={{base:"column",md:"row"}}>
+        <Stack  border="2px" borderColor={"silver"} mt="20px" borderRadius={"10px"} p="10px" bgColor={"rgb(0,34,79)"} direction={{base:"column",md:"row"}}>
         <Input value={place} onChange={(e)=>{setplace(e.target.value)}} bgColor={"white"} placeholder="Where are you going?"/>
         <Input bgColor={"white"} type={"date"}/>
         <Button onClick={handleclick} w={{base:"140px",md:"290px"}} bgColor={"white"}>Search</Button>
