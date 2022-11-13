14b75469d3af28ea2265d3f399a5094c195c5f97
Rajesh Byagalwar <byagalwarrajesh2001@gmail.com>
2022-11-13 13:37:28 +0530

    Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import AllRoutes from "./allRoutes/AllRoutes";
 import "./App.css";
 import { NavbarTop } from "./components/NavbarTop";
+import List from "./pages/list/List";
 
 function App() {
   return <div>
     <NavbarTop />
     <AllRoutes /> 
 
-    
+    {/* <List/> */}
   
   </div>;
 }
