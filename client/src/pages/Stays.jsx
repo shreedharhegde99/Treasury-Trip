613a9efd2e03f16dcdae4765f4d7ea9c61b7ea98
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-13 17:38:51 +0530

    added home component

--- client/src/pages/Stays.jsx
+++ client/src/pages/Stays.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavbarR from "../components/Navbar/NavbarR";
+import Home from "./Home2/Home";
 
 const Stays = () => {
   return (
     <div>
       <NavbarR />
-      stays
+      <Home />
     </div>
   );
 };
