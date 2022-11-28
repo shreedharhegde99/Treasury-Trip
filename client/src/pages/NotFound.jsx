5724153274c420483e7db12ca875557462539e8b
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-28 23:43:02 +0530

    updated image link

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -16,7 +16,7 @@ const NotFound = () => {
         <Image
           objectFit={"cover"}
           m="auto"
-          src="https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-20650.jpg?w=1060&t=st=1668319209~exp=1668319809~hmac=da1d1f5d2ffb74ed06b1415e5150afd9986a72884cfc27dad688df3bd3a10298"
+          src="https://i.imgur.com/HbBkrn9.jpg"
         />
       </Box>
     </Box>
